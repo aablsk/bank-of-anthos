3a81e8667f6206c3a115414fcbc9daeefa18b0b2
aablsk <bielski@google.com>
2022-07-22 09:07:36 +0000

    possible solution for running builds without skaffold cache

--- iac/tf/modules/ci-cd-pipeline/cloud-build-trigger.tf
+++ iac/tf/modules/ci-cd-pipeline/cloud-build-trigger.tf
@@ -2,7 +2,7 @@ resource "google_cloudbuild_trigger" "ci" {
   name = "${var.team}-ci"
   trigger_template {
     repo_name = var.source_repository.name
-    branch_name = "tf-v2"
+    branch_name = "^main$"
   }
   included_files = ["src/${var.team}/**", "src/components/**"]
   filename = var.pipeline_definition_filename
@@ -11,6 +11,7 @@ resource "google_cloudbuild_trigger" "ci" {
       _CACHE_URI = "gs://${google_storage_bucket.build_cache.name}/${google_storage_bucket_object.cache.name}"
       _CONTAINER_REGISTRY = "${var.container_registry.location}-docker.pkg.dev/${var.container_registry.project}/${var.container_registry.repository_id}"
       _SOURCE_STAGING_BUCKET = "gs://${google_storage_bucket.release_source_staging.name}/"
+      _CACHE = locals.cache_filename
   }
   service_account = google_service_account.cloud_build.id
 }
