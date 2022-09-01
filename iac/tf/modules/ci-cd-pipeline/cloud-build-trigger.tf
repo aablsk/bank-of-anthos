e352f40c3cbb80ad85b21b2923269ad976bdfd1a
aablsk <bielski@google.com>
2022-09-01 15:25:18 +0000

    fix source staging bucket

--- iac/tf/modules/ci-cd-pipeline/cloud-build-trigger.tf
+++ iac/tf/modules/ci-cd-pipeline/cloud-build-trigger.tf
@@ -15,7 +15,7 @@ resource "google_cloudbuild_trigger" "ci" {
       _TEAM = "${var.team}"
       _CACHE_URI = "gs://${google_storage_bucket.build_cache.name}/${google_storage_bucket_object.cache.name}"
       _CONTAINER_REGISTRY = "${var.container_registry.location}-docker.pkg.dev/${var.container_registry.project}/${var.container_registry.repository_id}"
-      _SOURCE_STAGING_BUCKET = "gs://${google_storage_bucket.release_source_staging.name}/"
+      _SOURCE_STAGING_BUCKET = "gs://${google_storage_bucket.release_source_staging.name}"
       _CACHE = local.cache_filename
   }
   service_account = google_service_account.cloud_build.id
