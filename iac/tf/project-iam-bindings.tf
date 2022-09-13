98ff638e0d10a693fe78f6850c1a5c5be2732d2b
aablsk <bielski@google.com>
2022-09-13 18:05:35 +0000

    fix cloudsql

--- iac/tf/project-iam-bindings.tf
+++ iac/tf/project-iam-bindings.tf
@@ -38,6 +38,10 @@ module "project-iam-bindings" {
     "roles/cloudsql.client" = [
         "serviceAccount:${google_service_account.gke_workload_staging.email}", # this implies that staging service account also has access to production CloudSQL. Could be solved by putting the CloudSQL instances in separate projects,
         "serviceAccount:${google_service_account.gke_workload_production.email}", # this implies that production service account also has access to staging CloudSQL. Could be solved by putting the CloudSQL instances in separate projects.
+    ],
+    "roles/cloudsql.instanceUser" = [
+        "serviceAccount:${google_service_account.gke_workload_staging.email}", # this implies that staging service account also has access to production CloudSQL. Could be solved by putting the CloudSQL instances in separate projects,
+        "serviceAccount:${google_service_account.gke_workload_production.email}", # this implies that production service account also has access to staging CloudSQL. Could be solved by putting the CloudSQL instances in separate projects.
     ]
   }
 }
